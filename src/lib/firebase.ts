5e2bae6019e74d8238592be75ce5e3d34cd69459
Layer-Labs-shop <layerlabscustomerservice@gmail.com>
2026-04-25 10:17:50 +0100

    Add Firebase configuration to firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB2IfMpmlFidX4U8n9VMRRoZbiKDrPCEDU",
+  authDomain: "layerlab-auth.firebaseapp.com",
+  projectId: "layerlab-auth",
+  storageBucket: "layerlab-auth.firebasestorage.app",
+  messagingSenderId: "148258695216",
+  appId: "1:148258695216:web:7cdeabcda3d3827316e5f5",
+  measurementId: "G-EQDJDD9LTZ"
+};
